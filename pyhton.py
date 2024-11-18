e3e74e9dad7cd1beffc7246dd3770cf2d7be1605
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-11-18 22:52:14 +0700

    menjalankan perintah sql unt membuat tabel nilai

--- pyhton.py
+++ pyhton.py
@@ -3,4 +3,17 @@ from tkinter import Tk, Label, Entry, Button, StringVar, messagebox, ttk
 
 # Fungsi untuk membuat database dan tabel
 def create_database():
-   
+    conn = sqlite3.connect('nilai_siswa.db')
+    cursor = conn.cursor()
+    #Menjalankan perintah sql untuk membuat tabel nilai
+    cursor.execute("""          
+        CREATE TABLE IF NOT EXISTS nilai_siswa (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nama_siswa TEXT,
+            biologi INTEGER,
+            fisika INTEGER,
+            inggris INTEGER,
+            prediksi_fakultas TEXT
+         )
+    """)
+    
